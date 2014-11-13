85420f0962e880b8a02aab221b75eed032b3abc1
amist <amistern0@gmail.com>
2014-11-13 19:46:43 +0200

    Inner functions convention

--- genetic_executor.py
+++ genetic_executor.py
@@ -13,29 +13,23 @@ class Population:
 		self.population.append(individual)
 		
 	def process_generation(self):
-		self.expand_population()
-		self.sort_population()
-		#self.distinct_population()
-		self.cut_population()
+		self._expand_population()
+		self._sort_population()
+		#self._distinct_population()
+		self._cut_population()
 		
-	def expand_population(self):
+	def _expand_population(self):
 		for i in range(self.size * (self.expansion_factor - 1)):
 			self.population.append(self.population[randint(0, len(self.population) - 1)].get_child(self.population[randint(0, len(self.population) - 1)]))
 		
-	def sort_population(self):
+	def _sort_population(self):
 		self.population.sort(key=lambda x: -x.get_fitness_value())
 		
-	def distinct_population(self):
+	def _distinct_population(self):
 		raise NotImplementedError
 		
-	def cut_population(self):
+	def _cut_population(self):
 		self.population = self.population[0:self.size - 1]
-		
-	def print_population(self):
-		print 'Printing population...'
-		for i in range(len(self.population)):
-			print 'printing plan number %d' % i
-			print self.population[i].gene
 			
 class Plan:
 	def get_random_gene(self):
