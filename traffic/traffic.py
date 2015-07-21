93e0d4d4c829a7a1eab4dba2b2744d00e99de837
amist <amistern0@gmail.com>
2015-07-21 16:49:35 +0300

    Started traffic simulation

---
+++ traffic/traffic.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import time
+
+class Car(object):
+    def __init__(self, lane, position, base_speed):
+        self.lane = lane
+        self.position = position
+        self.base_speed = base_speed
+        
+        self.speed = 0
+        self.acceleration = 10
+        
+    
+    def run(self):
+        self.position += self.speed
+        
+    
+    def accelerate(self):
+        if self.speed < self.base_speed:
+            self.speed += self.acceleration
+    
+        
+    def drive(self, car_front, car_back):
+        if car_front is None or car_front.position > self.position + self.speed:
+            self.run()
+            self.accelerate()
+        else:
+            self.speed = 0
+            
+        
+        
+        
+if __name__ == '__main__':
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.ion()
+    plt.show()
+    
+    cars = [None, Car(0, 2, 80), Car(0, 1, 90), Car(0, 0, 100), None]
+    for r in range(30):
+        print('-- round %d --' % r)
+        for i in range(len(cars)-1, 0, -1):
+            if cars[i] is not None:
+                cars[i].drive(cars[i-1], cars[i+1])
+            
+        
+        x_points = [car.position for car in cars if car is not None]
+        print(x_points)
+        y_points = [car.lane for car in cars if car is not None]
+        
+        plt.clf()
+        plt.plot(x_points, y_points, 'ro')
+        plt.axis((0,2000,-1,1))
+        plt.draw()
+        time.sleep(1)
+        
+        print('------------------')
+    
