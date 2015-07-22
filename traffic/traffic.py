d5cdb6725dc54f2f311c3b3f7ec1ab4899749e2a
amist <amistern0@gmail.com>
2015-07-22 19:07:46 +0300

    Added reports

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -13,7 +13,11 @@ class Car(object):
         self.last_lane_switch = 0
         self.patience = 100
         
-    
+        self.finish_time = -1
+        self.lane_switches = 0
+        self.finished = False
+        
+        
     def run(self):
         self.position += self.speed
         
@@ -21,6 +25,18 @@ class Car(object):
     def accelerate(self):
         if self.speed < self.base_speed:
             self.speed += self.acceleration
+    
+    
+    def switch_lane(self, lane):
+        self.lane = lane
+        self.lane_switches += 1
+        
+    
+    def get_report(self):
+        if self.finished:
+            return 'pos: %d, base speed: %d, finish time: %d, average speed: %.2f, lane switches: %d\n' % (self.position, self.base_speed, self.finish_time, self.position/float(self.finish_time), self.lane_switches)
+        else:
+            return 'lane: %d, pos: %d, speed: %d, base speed: %d\n' % (self.lane, self.position, self.speed, self.base_speed)
     
         
     def drive(self, front_car, back_car, right_lane, left_lane):
@@ -46,7 +62,8 @@ class Car(object):
                     lane_front_car = right_lane[lane_front_car_index-1]
                     #print('lane front car %d' % lane_front_car.position)
                 if (lane_back_car is None or lane_back_car.position + lane_back_car.speed <= self.position + self.speed - self.margin) and (lane_front_car is None or lane_front_car.position + lane_front_car.speed >= self.position + self.speed + self.margin) and (self.position - self.last_lane_switch > self.patience):
-                    self.lane -= 1
+                    #self.lane -= 1
+                    self.switch_lane(self.lane - 1)
                     self.last_lane_switch = self.position
         else:
             if left_lane is not None:
@@ -73,7 +90,8 @@ class Car(object):
                     #print('lane front car %d' % lane_front_car.position)
                     
                 if (lane_back_car is None or lane_back_car.position + lane_back_car.speed <= self.position + self.speed - self.margin) and (lane_front_car is None or lane_front_car.position + lane_front_car.speed >= self.position + self.speed + self.margin) and (self.position - self.last_lane_switch > self.patience):
-                    self.lane += 1
+                    #self.lane += 1
+                    self.switch_lane(self.lane + 1)
                     self.last_lane_switch = self.position
                     self.run()
                     self.accelerate()
@@ -88,9 +106,32 @@ class Road(object):
         self.lanes_number = lanes_number
         self.length = length
         self.cars = []
+        self.finished_cars = []
         
         self.lanes = [[] for _ in range(self.lanes_number)]
         self.clock = 0
+        
+    def add_car(self, car):
+        self.cars.append(car)
+        
+    def is_empty(self):
+        return len(self.cars) == 0
+        
+    def get_report(self):
+        ret = ''
+        ret += '--- Active Cars ---\n'
+        if len(self.cars) == 0:
+            ret += 'No active cars\n'
+        for car in self.cars:
+            ret += car.get_report()
+            
+        ret += '--- Finished Cars ---\n'
+        if len(self.finished_cars) == 0:
+            ret += 'No finished cars\n'
+        for car in self.finished_cars:
+            ret += car.get_report()
+            
+        return ret
         
     def step(self):
         #print([(car.lane, car.position, car.speed, car.base_speed) for car in self.cars])
@@ -116,15 +157,20 @@ class Road(object):
                 if j != len(self.lanes)-1:
                     left_lane = self.lanes[j+1]
                 self.lanes[j][i].drive(front_car, back_car, right_lane, left_lane)
+                if self.lanes[j][i].position > self.length:
+                    print('finished in position %d in time %d, base speed %d, average speed %.2f' % (self.lanes[j][i].position, self.clock, self.lanes[j][i].base_speed, (self.lanes[j][i].position/float(self.clock))))
+                    self.lanes[j][i].finish_time = self.clock
+                    self.lanes[j][i].finished = True
+                    self.finished_cars.append(self.lanes[j].pop(i))
                 
-        #self.cars = [car for lane in self.lanes for car in lane]
-        cars = []
-        for lane in self.lanes:
-            cars += lane
+        self.cars = [car for lane in self.lanes for car in lane]
+        #cars = []
+        #for lane in self.lanes:
+        #    cars += lane
         #print(-1, [car.position for car in cars])
-        cars.sort(key=lambda car: car.position, reverse=False)
+        self.cars.sort(key=lambda car: car.position, reverse=False)
         #print(0, [(car.position, car.speed) for car in cars])
-        self.cars = cars
+        #self.cars = cars
                 
         #for i in range(len(self.cars)-1, -1, -1):
         #    front_car = None
@@ -147,11 +193,11 @@ def run_animation(animate=True):
         plt.ion()
         plt.show()
     
-    road = Road(2, 2000)
+    road = Road(2, 1000)
     road.cars = [Car(0, 5, 4), Car(0, 4, 6), Car(0, 3, 5), Car(0, 2, 5), Car(0, 1, 6), Car(0, 0, 6),
                 Car(1, 5, 7), Car(1, 4, 8), Car(1, 3, 9), Car(1, 2, 8), Car(1, 1, 7), Car(1, 0, 9)]
     
-    while True:
+    while not road.is_empty():
         #print(road.clock)
         road.step()
         
@@ -174,7 +220,9 @@ def run_animation(animate=True):
                 plt.axis((0, road.length, -0.5, 1.5))
             plt.draw()
             #time.sleep(0.1)
+            
+    print(road.get_report())
         
 if __name__ == '__main__':
-    run_animation(animate=True)
+    run_animation(animate=False)
     
