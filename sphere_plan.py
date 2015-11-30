3951f95e12ad297a81a6f772d29c55579b8252cd
amist <amistern0@gmail.com>
2015-11-30 16:21:25 +0200

    Add sphere plan

---
+++ sphere_plan.py
@@ -0,0 +1,51 @@
+import random
+from genetic_executor import GeneticExecutor
+from genetic_executor import Plan
+
+class SpherePlan(Plan):
+    def __init__(self, size = 10, chromosome = None):
+        if (chromosome is None):
+            self.chromosome = []
+        else:
+            self.chromosome = chromosome
+        self.size = size
+        self._fitness = None
+        self._lower_bound = -5.12
+        self._upper_bound = 5.12
+        
+        
+    def get_random_chromosome(self):
+        chromosome = []
+        for _ in range(self.size):
+            chromosome.append(random.uniform(self._lower_bound, self._upper_bound))
+        return chromosome
+            
+            
+    def get_child(self, parent2):
+        child = SpherePlan(self.size)
+        for i in range(self.size):
+            # crossover
+            child.chromosome.append(self.chromosome[i] if random.randint(0, 1) == 0 else parent2.chromosome[i])
+            
+            # mutate
+            if random.randint(1, 10 * self.size) == 1:
+                child.chromosome[i] = random.uniform(self._lower_bound, self._upper_bound)
+        return child
+        
+        
+    def get_fitness_value(self):
+        if self._fitness is not None:
+            return self._fitness
+        return -sum([x*x for x in self.chromosome])
+        
+        
+    def print_plan(self):
+        print(self.chromosome)
+            
+        
+        
+if __name__ == '__main__':
+    sp = SpherePlan(10)
+    ge = GeneticExecutor(sp, initial_population_size = 10, max_generations_number = 100)
+    solution = ge.get_solution()
+    solution.print_plan()
