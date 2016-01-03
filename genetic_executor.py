7baadc5c390d7a81cccdf98120713fe9400249cf
amist <amistern0@gmail.com>
2016-01-03 20:04:14 +0200

    A small refactoring

--- genetic_executor.py
+++ genetic_executor.py
@@ -10,8 +10,17 @@ class Population:
         self.expansion_factor = expansion_factor
         self.coindividuals_num = 100
         
+        
+    def init_random_population(self, individual):
+        for _ in range(self.size):
+            plan = copy.deepcopy(individual)
+            plan.chromosome = plan.get_random_chromosome()
+            self.population.append(plan)
+        
+        
     def _get_individual_with_uniform_choice(self):
         return self.population[random.randint(0, len(self.population) - 1)]
+        
         
     def _get_individual_with_moving_window(self, percentage):
         window_size = self.size / 10
@@ -22,6 +31,7 @@ class Population:
         #print(percentage, lb, ub)
         return self.population[random.randint(lb, ub)]
         
+        
     def _get_coindividual_with_uniform_choice(self, parent1):
         parent2 = None
         max_fitness = None
@@ -31,6 +41,7 @@ class Population:
                 parent2 = other
                 max_fitness = parent1.get_cofitness_value(other)
         return parent2
+        
         
     def _get_individual_with_weighted_choice(self):
         choices = self.population
@@ -50,8 +61,10 @@ class Population:
             upto += f
         assert False, "In _get_individual_with_weighted_choice. Shouldn't get here"
         
+        
     def _calculate_fitness_concurrently(self, individual, i, res_dict):
         res_dict[i] = individual.get_fitness_value()
+        
         
     def _calculate_fintesses(self):
         manager = multiprocessing.Manager()
@@ -66,8 +79,6 @@ class Population:
         for i in range(len(self.population)):
             self.population[i]._fitness = res_dict[i]
         
-    def add_individual(self, individual):
-        self.population.append(individual)
         
     def process_generation(self):
         self._expand_population()
@@ -76,6 +87,7 @@ class Population:
         #self._distinct_population()
         self._cut_population()
         #self.population[0].print_plan()
+        
         
     def _expand_population(self):
         #print("========================")
@@ -88,31 +100,40 @@ class Population:
             self.population.append(child)
             #self.population.append(self._get_individual_with_weighted_choice().get_child(self._get_individual_with_weighted_choice()))
         
+        
     def _sort_population(self):
         self.population.sort(key=lambda x: -x.get_fitness_value())
+        
         
     def _distinct_population(self):
         raise NotImplementedError
         
+        
     def _cut_population(self):
         self.population = self.population[0:self.size - 1]
+            
             
 class Plan:
     def get_random_chromosome(self):
         raise NotImplementedError('You have to implemtent a get_random_chromosome function')
     
+    
     def get_child(self, parent2):
         raise NotImplementedError('You have to implemtent a get_child function')
+        
         
     def get_fitness_value(self):
         raise NotImplementedError('You have to implemtent a get_fitness_value function')
         
+        
     def get_optimal_value(self):
         return None
+        
         
     def print_plan(self):
         return
 
+        
 class GeneticExecutor:
 
     def __init__(self, individual_instance, population_size=200, max_generations_number=100, debug=False):
@@ -121,12 +142,10 @@ class GeneticExecutor:
         self.max_generations_number = max_generations_number
         self.debug = debug
         
+        
     def get_solution(self):
         population = Population(size=self.population_size)
-        for i in range(self.population_size):
-            plan = copy.deepcopy(self.individual_instance)
-            plan.chromosome = plan.get_random_chromosome()
-            population.add_individual(plan)
+        population.init_random_population(self.individual_instance)
         
         for i in range(self.max_generations_number):
             if self.debug == True:
